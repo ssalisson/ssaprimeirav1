1d91d9a6c0f2ae129f2f30a23c4239203cceaac3
ssalisson <italobielplayer@gmail.com>
2026-01-31 17:27:57 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -115,6 +115,8 @@ function calcularNota3() {
       if (nota3Necessaria > 100) {
           resultadoDiv.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Nota necessária no SSA-3: ' + nota3Necessaria.toFixed(2);
           resultadoDiv.style.backgroundColor = "rgb(255, 255, 163)"; 
+          resultadoDiv.style.fontSize = "20px";
+          resultadoDiv.style.fontWeight = "bold";
       } else if (nota3Necessaria < 0) {
           resultadoDiv.innerHTML = '<i class="fa-solid fa-triangle-exclamation"></i> Você já passou!';
           resultadoDiv.style.backgroundColor = "rgb(255, 255, 163)"; 
